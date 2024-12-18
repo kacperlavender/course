cd08d4bac9ec424b24061fe4b24d3c49a71cc820
Kacper <kacper.kielbasa5000@gmail.com>
2024-12-18 17:29:16 +0100

    commit

---
+++ multithreading.py
@@ -0,0 +1,3 @@
+# multithreading = used to perform multiple tash concurrently (multitasking)
+#       Good for I/O bound tasks ike reading files or fetching data from APIs
+#       threading. Thread(target=my_function)
