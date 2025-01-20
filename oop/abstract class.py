3638048348354dbe15a9376e91e9a1f6b6459139
Kacper <kacper.kielbasa5000@gmail.com>
2025-01-20 01:14:15 +0100

    commit

---
+++ oop/abstract class.py
@@ -0,0 +1,11 @@
+'''
+
+abstract class: a class that cannot be instantianted by its own;
+                meant to be subclassed;
+                they can contain abstract methods, which are declared 
+                            but have no implementation
+                1. prevents instantiation of the class itself
+                2. requires children to use inherited abstract methods
+'''
+
+# 35;45
